89c34c8bf1bf73075ad605cd735c79001b86b08c
asdf99245 <asdf99245@naver.com>
2023-07-08 21:29:04 +0900

    chore: check type script 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,5 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return (
-    <main className={inter.className}>main</main>
-  );
+  return <main className={inter.className}>main</main>;
 }
