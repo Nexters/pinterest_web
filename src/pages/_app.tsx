2873c9222f05cdc3c7c41d784c686dd00e9c25f7
asdf99245 <asdf99245@naver.com>
2023-07-15 15:56:35 +0900

    chore: test console 삭제

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,5 @@ import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const a = 1;
-  console.log(a);
   return <Component {...pageProps} />;
 }
