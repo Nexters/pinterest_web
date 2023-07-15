ec11ab4004f3f7d9552fd5d776c37bebfe7006f0
asdf99245 <asdf99245@naver.com>
2023-07-15 15:49:25 +0900

    test husky

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,7 @@ import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const a = 1;
+  console.log(a);
   return <Component {...pageProps} />;
 }
