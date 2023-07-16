01beee3166b8fb9be840d515a3632fcb9d202299
asdf99245 <asdf99245@naver.com>
2023-07-16 18:44:36 +0900

    feat: pretendard, monserrat 폰트 모듈 추가

---
+++ src/styles/fonts.ts
@@ -0,0 +1,32 @@
+import { Montserrat } from 'next/font/google';
+import localFont from 'next/font/local';
+
+const pretendard = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Pretendard-Regular.subset.woff2',
+      weight: '400',
+    },
+    {
+      path: '../../public/fonts/Pretendard-SemiBold.subset.woff2',
+      weight: '600',
+    },
+    {
+      path: '../../public/fonts/Pretendard-Bold.subset.woff2',
+      weight: '700',
+    },
+  ],
+  variable: '--font-pretendard',
+  style: 'normal',
+  display: 'swap',
+});
+
+const montserrat = Montserrat({
+  variable: '--font-montserrat',
+  weight: ['400', '600'],
+  style: 'normal',
+  display: 'swap',
+  subsets: ['latin'],
+});
+
+export { pretendard, montserrat };
