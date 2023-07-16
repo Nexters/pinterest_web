39fc555baaad0744f0f47df5f7ec53da7dc0ac2c
asdf99245 <asdf99245@naver.com>
2023-07-16 19:33:24 +0900

    chore: sample typography

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
-  return <main className={inter.className}>main</main>;
+  return (
+    <main>
+      <p className='font-pretendard text-main-headline'>이건 메인 헤드라인이에요 Pretendard</p>
+      <p className='font-pretendard text-sub-headline'>이건 서브 헤드라인이에요 Pretendard</p>
+      <p className='font-pretendard text-body1'>이건 본문1이에요 Pretendard</p>
+      <p className='font-pretendard text-body2'>이건 본문2이에요 Pretendard</p>
+      <p className='font-pretendard text-caption'>이건 캡션이에요 Pretendard</p>
+      <p className='font-pretendard text-button1'>이건 버튼1이에요 Pretendard</p>
+      <p className='font-pretendard text-button2'>이건 버튼2이에요 Pretendard</p>
+      <p className='font-montserrat text-accent-eng'>Montserrat accent</p>
+      <p className='font-montserrat text-body-eng'>Montserrat body</p>
+      <p className='font-montserrat text-caption-eng'>Montserrat caption</p>
+    </main>
+  );
 }
