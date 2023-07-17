c5d7d046fa02b65a154f16b765f70e16d5e9100d
asdf99245 <asdf99245@naver.com>
2023-07-17 23:55:57 +0900

    feat: Icon 컴포넌트 구현

---
+++ src/components/shared/icon/Icon.tsx
@@ -0,0 +1,26 @@
+import { SVGProps } from 'react';
+import clsx from 'clsx';
+import * as icons from '@/assets/icons';
+
+const RIGHT_ARROW_WIDTH = 40;
+const DEFAULT_SIZE = 24;
+
+type IconType = keyof typeof icons;
+
+interface Props extends SVGProps<SVGSVGElement> {
+  iconType: IconType;
+}
+
+export function Icon({
+  iconType,
+  color,
+  width = iconType === 'RightArrow' ? RIGHT_ARROW_WIDTH : DEFAULT_SIZE,
+  height = DEFAULT_SIZE,
+  ...restProps
+}: Props) {
+  const Icon = icons[iconType];
+
+  return (
+    <Icon fill={color} width={width} height={height} className={clsx({ 'fill-primary': !color })} {...restProps} />
+  );
+}
