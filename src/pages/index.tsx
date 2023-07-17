5560a770e539b05f48ea4bcceb432912143452da
asdf99245 <asdf99245@naver.com>
2023-07-18 00:23:20 +0900

    chore: sample text example 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,16 @@ import { Icon } from '@/components/shared';
 export default function Home() {
   return (
     <main>
-      <p className='font-pretendard text-main-headline'>이건 메인 헤드라인이에요 Pretendard</p>
-      <p className='font-pretendard text-sub-headline'>이건 서브 헤드라인이에요 Pretendard</p>
-      <p className='font-pretendard text-body1'>이건 본문1이에요 Pretendard</p>
-      <p className='font-pretendard text-body2'>이건 본문2이에요 Pretendard</p>
-      <p className='font-pretendard text-caption'>이건 캡션이에요 Pretendard</p>
-      <p className='font-pretendard text-button1'>이건 버튼1이에요 Pretendard</p>
-      <p className='font-pretendard text-button2'>이건 버튼2이에요 Pretendard</p>
-      <p className='font-montserrat text-accent-eng'>Montserrat accent</p>
-      <p className='font-montserrat text-body-eng'>Montserrat body</p>
-      <p className='font-montserrat text-caption-eng'>Montserrat caption</p>
+      <p className='text-main-headline'>이건 메인 헤드라인이에요 Pretendard</p>
+      <p className='text-sub-headline'>이건 서브 헤드라인이에요 Pretendard</p>
+      <p className='text-body1'>이건 본문1이에요 Pretendard</p>
+      <p className='text-body2'>이건 본문2이에요 Pretendard</p>
+      <p className='text-caption'>이건 캡션이에요 Pretendard</p>
+      <p className='text-button1'>이건 버튼1이에요 Pretendard</p>
+      <p className='text-button2'>이건 버튼2이에요 Pretendard</p>
+      <p className='text-accent-eng'>Montserrat accent</p>
+      <p className='text-body-eng'>Montserrat body</p>
+      <p className='text-caption-eng'>Montserrat caption</p>
       <Icon iconType='Edit' />
       <Icon iconType='Menu' />
       <Icon iconType='Plus' />
