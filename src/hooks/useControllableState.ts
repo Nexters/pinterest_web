7b2221cc8837b275898500f48f5cc911d3b5169f
asdf99245 <asdf99245@naver.com>
2023-07-19 01:48:22 +0900

    feat: useControllableState hook 추가

---
+++ src/hooks/useControllableState.ts
@@ -0,0 +1,27 @@
+import { useCallback, useState } from 'react';
+
+interface Props<T> {
+  defaultState: T;
+  state?: T;
+  onChange?: (value: T) => void;
+}
+
+export function useControllableState<T>({ defaultState, state: valueFromProps, onChange }: Props<T>) {
+  const [uncontrolledValue, setUncontrolledValue] = useState<T>(defaultState);
+
+  const isControlled = valueFromProps !== undefined;
+  const value = isControlled ? valueFromProps : uncontrolledValue;
+
+  const onValueChange = useCallback(
+    (nextState: T) => {
+      // 비제어 컴포넌트일 때만 내부 상태 변경
+      if (!isControlled) {
+        setUncontrolledValue(nextState);
+      }
+      onChange?.(nextState);
+    },
+    [isControlled, onChange],
+  );
+
+  return [value, onValueChange] as const;
+}
