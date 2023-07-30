a2f22994a7f6c1eac92b5525caa4a76a892e45b1
bsy1141 <bsy1141@naver.com>
2023-07-30 22:40:31 +0900

    feat: Input 컴포넌트에 captionPosition props 추가

---
+++ src/components/shared/Modal/Modal.tsx
@@ -0,0 +1,40 @@
+import type { MouseEventHandler, PropsWithChildren } from 'react';
+import { Portal } from '@/components/shared';
+
+interface Props {
+  isOpen?: boolean;
+  title: string;
+  onCancel?: MouseEventHandler<HTMLElement>;
+  onSave?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export function Modal({ children, isOpen, title, onCancel, onSave }: PropsWithChildren<Props>) {
+  if (!isOpen) return null;
+
+  return (
+    <Portal>
+      <div className='tw-fixed tw-left-0 tw-top-0 tw-h-full tw-w-full tw-bg-black tw-opacity-60' onClick={onCancel} />
+      <div className='tw-translate tw-fixed tw-left-1/2 tw-top-1/2 tw-flex tw-h-[432px] tw-w-[336px] tw--translate-x-1/2 tw--translate-y-1/2 tw-flex-col tw-items-center tw-rounded tw-bg-white tw-px-5 tw-pb-6 tw-shadow-lg'>
+        <div className='tw-text-main-headline tw-h-[88px] tw-pb-[30px] tw-pt-[38px]'>
+          <h2>{title}</h2>
+        </div>
+        <div className='tw-flex tw-w-full tw-flex-1 tw-flex-col tw-gap-6'>{children}</div>
+        <div className='tw-gap tw-flex tw-w-full tw-items-center tw-gap-3'>
+          {/* TODO: button 컴포넌트 완성되면 붙이기 */}
+          <button
+            className='tw-text-button1 tw-flex-1 tw-rounded tw-bg-grayscale-200 tw-py-[14px] tw-text-grayscale-600'
+            onClick={onCancel}
+          >
+            취소
+          </button>
+          <button
+            className='tw-text-button1 tw-flex-1 tw-rounded tw-bg-grayscale-600 tw-py-[14px] tw-text-grayscale-200'
+            onClick={onSave}
+          >
+            저장
+          </button>
+        </div>
+      </div>
+    </Portal>
+  );
+}
--- src/components/shared/Textarea/Textarea.tsx
+++ src/components/shared/Textarea/Textarea.tsx
@@ -1,14 +1,14 @@
-import type { ChangeEventHandler, InputHTMLAttributes } from 'react';
+import type { ChangeEventHandler, TextareaHTMLAttributes } from 'react';
 import { forwardRef, useId } from 'react';
 import { useControllableState } from '@/hooks';
 import { cn } from '@/utils/cn';
 
-interface Props extends InputHTMLAttributes<HTMLInputElement> {
+const TEXTAREA_LINES = 4;
+
+interface Props extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   label?: string;
   value?: string;
-  captionPosition?: 'top' | 'bottom';
   caption?: string;
-  feedback?: string;
   defaultValue?: string;
   onValueChange?: (value: string) => void;
 }
@@ -17,24 +17,22 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {
  * value를 전달하지 않으면 내부에서 상태가 관리됩니다.
  * @example
  * ```tsx
- * <Input label='Label' placeholder='placeholder text' maxLength={10} />
+ * <Textarea label='Label' placeholder='placeholder text' maxLength={10} />
  * ```
  * value를 전달하여 외부에서 상태를 제어할 수 있습니다.
  * @example
  * ```tsx
  * const [value, setValue] = useState('');
- * return <Input label='Label' placeholder='placeholder text' maxLength={10} value={value} onValueChange={(value) => setValue(value)} />
+ * return <Textarea label='Label' placeholder='placeholder text' maxLength={10} value={value} onValueChange={(value) => setValue(value)} />
  * ```
  */
-export const Input = forwardRef<HTMLInputElement, Props>(
+export const Textarea = forwardRef<HTMLTextAreaElement, Props>(
   (
     {
       id: idFromProps,
       label,
       value: valueFromProps,
-      captionPosition = 'bottom',
       caption,
-      feedback,
       defaultValue = '',
       maxLength,
       onValueChange,
@@ -50,7 +48,7 @@ export const Input = forwardRef<HTMLInputElement, Props>(
     });
     const id = useId();
 
-    const handleInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const handleInputChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
       if (maxLength && e.target.value.length > maxLength) {
         return;
       }
@@ -61,11 +59,8 @@ export const Input = forwardRef<HTMLInputElement, Props>(
       <div className='tw-flex tw-flex-col tw-text-black'>
         {label && (
           <div className='tw-mb-3 tw-flex tw-w-full tw-items-center'>
-            <label htmlFor={idFromProps ?? id} className='tw-flex tw-items-center tw-gap-2.5'>
-              <h1 className='tw-text-accent-eng'>{label}</h1>
-              {caption && captionPosition === 'top' && (
-                <p className='tw-text-caption tw-text-grayscale-400'>{caption}</p>
-              )}
+            <label htmlFor={idFromProps ?? id} className='tw-text-accent-eng'>
+              {label}
             </label>
             {maxLength && (
               <span className='tw-text-body2 tw-ml-auto tw-text-grayscale-400'>
@@ -74,25 +69,23 @@ export const Input = forwardRef<HTMLInputElement, Props>(
             )}
           </div>
         )}
-        <input
+        <textarea
           ref={ref}
           id={idFromProps ?? id}
           value={value}
           maxLength={maxLength}
           onChange={handleInputChange}
+          rows={TEXTAREA_LINES}
           className={cn(
-            'tw-text-body2 tw-w-full tw-rounded tw-border tw-border-transparent tw-bg-grayscale-100 tw-px-2.5 tw-py-2 tw-outline-none placeholder:tw-text-grayscale-300 focus:tw-border-grayscale-300 active:tw-border-grayscale-300',
+            'tw-text-body2 tw-h-[100px] tw-w-full tw-resize-none tw-overflow-scroll tw-rounded tw-border tw-border-transparent tw-bg-grayscale-100 tw-px-2.5 tw-py-2 tw-outline-none tw-scrollbar-hide placeholder:tw-text-grayscale-300 focus:tw-border-grayscale-300 active:tw-border-grayscale-300',
             className,
           )}
           {...restProps}
         />
-        {caption && captionPosition === 'bottom' && (
-          <p className='tw-text-caption tw-mt-1 tw-text-grayscale-400'>{caption}</p>
-        )}
-        {feedback && <p className='tw-text-danger tw-text-caption tw-mt-1'>{feedback}</p>}
+        <p className='tw-text-caption tw-mt-1 tw-text-grayscale-400'>{caption}</p>
       </div>
     );
   },
 );
 
-Input.displayName = 'Input';
+Textarea.displayName = 'Textarea';
