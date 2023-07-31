b1573c9d5c5e2610ecc968623226cdc30736ef28
asdf99245 <asdf99245@naver.com>
2023-08-01 02:42:16 +0900

    feat: close, left-chevron icon 추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,6 +1,8 @@
+export { default as Close } from './close.svg';
 export { default as Edit } from './edit.svg';
 export { default as Info } from './info.svg';
 export { default as Menu } from './menu.svg';
 export { default as Plus } from './plus.svg';
 export { default as RightArrow } from './right-arrow.svg';
 export { default as RightChevron } from './right-chevron.svg';
+export { default as LeftChevron } from './left-chevron.svg';
