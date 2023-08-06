96ec44a050e177cb634a6054c90402a299c1d021
SeoYoung Bae <60652298+bsy1141@users.noreply.github.com>
2023-08-06 22:33:54 +0900

    feature: 아이템 페이지 퍼블리싱 (#21)
    * feat: 아이템 슬라이드뷰 퍼블리싱
    
    * feat: 아이템 추가 페이지 퍼블리싱
    
    * feat: edit 페이지 퍼블리싱 완료
    
    * docs: 클래스 이름 삭제
    
    * rename: 디렉토리명 수정
    
    * feat: 디렉토리명 수정
    
    * feat: 코드리뷰 반영
    
    * fix: 저장 후 디테일 페이지로 이동하도록 수정
    
    ---------
    
    Co-authored-by: asdf99245 <asdf99245@naver.com>

---
+++ src/components/shared/ImageFrame/index.ts
@@ -0,0 +1,1 @@
+export { ImageFrame } from './ImageFrame';
---
+++ src/components/user/item/ItemsSlide/ItemsSlide.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react';
+import { Carousel } from 'react-bootstrap';
+import { Icon, ImageFrame } from '@/components/shared';
+
+interface ItemType {
+  id: number;
+  title: string;
+  image: string;
+  description: string;
+}
+
+interface ItemSlideProps {
+  items: ItemType[];
+  activeIndex: number;
+  setActiveIndex: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export function ItemsSlide({ items, activeIndex, setActiveIndex }: ItemSlideProps) {
+  const handleSelect = (selectedIndex: number) => {
+    setActiveIndex(selectedIndex);
+  };
+
+  useEffect(() => {
+    require('bootstrap/dist/js/bootstrap.js');
+  }, []);
+
+  // const goToNext = () => {
+  //   const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+  //   setActiveIndex(nextIndex);
+  // };
+  // const goToPrev = () => {
+  //   const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+  //   setActiveIndex(nextIndex);
+  // };
+
+  return (
+    <>
+      <Carousel activeIndex={activeIndex} onSelect={handleSelect} controls={true} indicators={false}>
+        {items.map((item) => (
+          <Carousel.Item key={item.title}>
+            <ImageFrame alt={item.title} src={item.image} />
+          </Carousel.Item>
+        ))}
+        {/* <button onClick={goToPrev} className='absolute left-4 top-1/2'>
+        <Icon iconType='LeftChevronBG' width='2rem' height='2rem' />
+      </button>
+      <button onClick={goToNext} className='absolute right-4 top-1/2'>
+        <Icon iconType='RightChevronBG' width='2rem' height='2rem' />
+      </button> */}
+      </Carousel>
+    </>
+  );
+}
