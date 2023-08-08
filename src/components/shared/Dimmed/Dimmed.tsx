e5b4ccd0b0a083aee97fa9a88c5dc660687ba619
asdf99245 <asdf99245@naver.com>
2023-08-09 01:35:39 +0900

    design: Dimmed z-index 부여

--- src/components/shared/Dimmed/Dimmed.tsx
+++ src/components/shared/Dimmed/Dimmed.tsx
@@ -3,5 +3,7 @@ import type { HTMLAttributes } from 'react';
 type Props = HTMLAttributes<HTMLDivElement>;
 
 export function Dimmed(props: Props) {
-  return <div className='tw-fixed tw-left-0 tw-top-0 tw-h-full tw-w-full tw-bg-black tw-opacity-60' {...props} />;
+  return (
+    <div className='tw-fixed tw-left-0 tw-top-0 tw-z-10 tw-h-full tw-w-full tw-bg-black tw-opacity-60' {...props} />
+  );
 }
