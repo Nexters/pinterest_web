95b907e51ab5457d37a77017411eaebecc6bd047
asdf99245 <asdf99245@naver.com>
2023-08-12 20:03:21 +0900

    feat: 유저 페이지 모달 연결

--- src/pages/user/[id]/index.tsx
+++ src/pages/user/[id]/index.tsx
@@ -1,10 +1,12 @@
 import { Avatar, Button, Icon } from '@/components/shared';
 import { Drawer } from '@/components/shared/Drawer';
-import { CameraRoll } from '@/components/user/CameraRoll';
-import { useToggle } from '@/hooks/useToggle';
+import { AddMenu } from '@/components/user';
+import { CameraRoll, FilmAddModal, FilmSelectModal, FilmTitleModal } from '@/components/user';
+import { useModalManager } from '@/components/user/hooks';
 
 export default function User() {
-  const { status: isDrawerOpen, setOn: openDrawer, setOff: closeDrawer } = useToggle();
+  const { status, dispatch } = useModalManager();
+  const { isDrawerOpen, isAddMenuOpen, isFilmAddModalOpen, isFilmSelectModalOpen, isFilmTitleModalOpen } = status;
 
   return (
     <div className='tw-relative tw-overflow-x-hidden tw-pb-10 tw-pt-3'>
@@ -18,17 +20,30 @@ export default function User() {
         <CameraRoll title='최근에 간 카페' />
         <CameraRoll title='고양이짤들' />
       </div>
-      <Button variant='rounded' className='tw-fixed tw-bottom-5 tw-right-5'>
+      <Button
+        variant='rounded'
+        className='tw-fixed tw-bottom-5 tw-right-5'
+        onClick={() => dispatch({ type: 'OPEN_ADD_MENU' })}
+      >
         ADD
       </Button>
       <Icon
         iconType='Menu'
-        onClick={openDrawer}
+        onClick={() => dispatch({ type: 'OPEN_DRAWER' })}
         className='tw-absolute tw-right-3.5 tw-top-2.5 tw-cursor-pointer'
         width={32}
         height={32}
       />
-      <Drawer isOpen={isDrawerOpen} onClose={closeDrawer} />
+      <FilmTitleModal isOpen={isFilmTitleModalOpen} onCancel={() => dispatch({ type: 'CLOSE_FILM_TITLE_MODAL' })} />
+      <FilmAddModal isOpen={isFilmAddModalOpen} onCancel={() => dispatch({ type: 'CLOSE_FILM_ADD_MODAL' })} />
+      <FilmSelectModal isOpen={isFilmSelectModalOpen} onCancel={() => dispatch({ type: 'CLOSE_FILM_SELECT_MODAL' })} />
+      <AddMenu
+        isOpen={isAddMenuOpen}
+        onClose={() => dispatch({ type: 'CLOSE_ADD_MENU' })}
+        onAddFilm={() => dispatch({ type: 'OPEN_FILM_ADD_MODAL' })}
+        onUploadPhoto={() => dispatch({ type: 'OPEN_FILM_SELECT_MODAL' })}
+      />
+      <Drawer isOpen={isDrawerOpen} onClose={() => dispatch({ type: 'CLOSE_DRAWER' })} />
     </div>
   );
 }
