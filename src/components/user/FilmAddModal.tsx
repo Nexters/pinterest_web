0424c3f4b857318724d851ff74cfcbfb190ad86f
asdf99245 <asdf99245@naver.com>
2023-08-13 05:25:24 +0900

    feat: Film 추가 API 연동

--- src/components/user/FilmAddModal.tsx
+++ src/components/user/FilmAddModal.tsx
@@ -1,9 +1,13 @@
+import { useRouter } from 'next/router';
 import { type ComponentProps, type MouseEventHandler, useState } from 'react';
+import { useCreateFilms } from '@/query-hooks/useFilms';
 import { Input, Modal } from '@/components/shared';
 
 type Props = Omit<ComponentProps<typeof Modal>, 'title' | 'children'>;
 
 export function FilmAddModal({ onCancel, ...restProps }: Props) {
+  const router = useRouter();
+  const { mutate: createFilms } = useCreateFilms();
   const [input, setInput] = useState('');
 
   const handleValueChange = (value: string) => {
@@ -11,13 +15,30 @@ export function FilmAddModal({ onCancel, ...restProps }: Props) {
   };
 
   const handleSave: MouseEventHandler<HTMLButtonElement> = (e) => {
-    // TODO: 필름 추가 API 연결
+    const userId = localStorage.getItem('userId');
+    if (!userId) {
+      router.push('/signin');
+      return;
+    }
+
+    createFilms({ title: input, userId });
+
     if (onCancel) onCancel(e);
   };
 
   return (
-    <Modal title='새로운 필름 추가' onCancel={onCancel} onSave={handleSave} {...restProps}>
-      <Input label='필름 제목' placeholder='필름 제목을 입력하세요' value={input} onValueChange={handleValueChange} />
+    <Modal
+      title='새로운 필름 추가'
+      onCancel={onCancel}
+      onSave={handleSave}
+      {...restProps}
+    >
+      <Input
+        label='필름 제목'
+        placeholder='필름 제목을 입력하세요'
+        value={input}
+        onValueChange={handleValueChange}
+      />
     </Modal>
   );
 }
