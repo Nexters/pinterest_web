603bf90255383398e813634922fd6b651d9cf929
asdf99245 <asdf99245@naver.com>
2023-08-15 15:13:54 +0900

    feat: Avatar description 표시

--- src/pages/user/[id]/index.tsx
+++ src/pages/user/[id]/index.tsx
@@ -78,6 +78,7 @@ export default function User({
         <Avatar
           src={userData.profile_img ?? '/images/avatar-placeholder.png'}
           nickname={userData.name}
+          description={userData.text ?? ''}
           viewCount={userData.visitors}
           isLogin={isLogin}
           displayMeta
