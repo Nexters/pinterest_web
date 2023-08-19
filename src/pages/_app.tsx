fd7d9911e5d67b0a16391ed2b15659ab1e85028a
Yunkyu Jung <39851220+asdf99245@users.noreply.github.com>
2023-08-19 11:26:55 +0900

    fix: mobile view css (#42)
    * feat: viewport 확대 불가능하도록 수정
    
    인풋 클릭시 확대되는 현상 수정
    
    * feat: 웹뷰 css 설정
    
    - 버튼 터치 음영 제거
    - 요소 select 방지
    - 모바일 롱터치 방지

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Hydrate, ModalProvider, QueryProvider } from '@/providers';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { montserrat, pretendard } from '@/styles/fonts';
@@ -6,14 +7,22 @@ import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryProvider>
-      <ModalProvider>
-        <Hydrate state={pageProps.dehydratedState}>
-          <main className={`${pretendard.variable} ${montserrat.variable}`}>
-            <Component {...pageProps} />
-          </main>
-        </Hydrate>
-      </ModalProvider>
-    </QueryProvider>
+    <>
+      <Head>
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
+        />
+      </Head>
+      <QueryProvider>
+        <ModalProvider>
+          <Hydrate state={pageProps.dehydratedState}>
+            <main className={`${pretendard.variable} ${montserrat.variable}`}>
+              <Component {...pageProps} />
+            </main>
+          </Hydrate>
+        </ModalProvider>
+      </QueryProvider>
+    </>
   );
 }
